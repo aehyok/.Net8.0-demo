17771e685ef1b43a3dcf9322b653c6154f93197d
刘启明 <455043818@qq.com>
2023-11-29 09:44:46 +0800

    feat: 实现一个BackgroundService

---
+++ aehyok.Schedules/CronScheduleService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.Schedules
+{
+    /// <summary>
+    /// 实现一个后台服务
+    /// </summary>
+    public class CronScheduleService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Test CronSchedule");
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                Thread.Sleep(1000);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- aehyok.Schedules/Program.cs
+++ aehyok.Schedules/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using aehyok.Schedules;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 Console.WriteLine("Hello, World!");
+
+var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                //注册后台服务
+                services.AddHostedService<CronScheduleService>();
+            })
+            .Build();
+
+// 运行主机
+await host.RunAsync();
