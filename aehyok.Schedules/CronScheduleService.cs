f9bd5f7afbbe7c4294667b5e7f6e7450caf89456
刘启明 <455043818@qq.com>
2023-11-29 10:55:44 +0800

    feat：单个定时任务搭建成功

--- aehyok.Schedules/CronScheduleService.cs
+++ aehyok.Schedules/CronScheduleService.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Cronos;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +13,60 @@ namespace aehyok.Schedules
     /// <summary>
     /// 实现一个后台服务
     /// </summary>
-    public class CronScheduleService : BackgroundService
+    public class CronScheduleService: BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        /// <summary>
+        /// 定时表达式
+        /// </summary>
+        private string Expression = "0/10 * * * * ? ";
+
+
+        public DateTime? GetNextTime()
         {
-            Console.WriteLine("Test CronSchedule");
-            while(!stoppingToken.IsCancellationRequested)
+            return  CronExpression.Parse(Expression, CronFormat.IncludeSeconds).GetNextOccurrence(DateTime.UtcNow, TimeZoneInfo.Local);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // CronFormat.IncludeSeconds 表达式中秒字段必须被指定
+            var nextExcuteTime = GetNextTime();
+
+            if (nextExcuteTime.HasValue)
             {
-                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-                Thread.Sleep(1000);
+                Console.WriteLine($"任务下次执行时间:{nextExcuteTime.Value.ToLocalTime()}");
             }
-            return Task.CompletedTask;
+
+
+            ///用于准确测量时间间隔
+            Stopwatch stopwatch = new Stopwatch();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if(DateTimeOffset.UtcNow < nextExcuteTime)
+                {
+                    // 延迟重新执行
+                    await Task.Delay(1000, stoppingToken);
+                    continue;
+                }
+
+                try
+                {
+                    stopwatch.Restart();
+                    Console.WriteLine("开始执行任务");
+                    Console.WriteLine($"任务执行时间:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                    Console.WriteLine("任务执行完成");
+                }
+                catch
+                {
+
+                }
+
+                nextExcuteTime = GetNextTime();
+                if(nextExcuteTime.HasValue)
+                {
+                    Console.WriteLine($"任务下次执行时间:{nextExcuteTime.Value.ToLocalTime()}");
+                }
+            }
         }
     }
 }
