338fd68d701a00e69bd777bfb43db5d6e50a2d84
刘启明 <455043818@qq.com>
2023-12-13 00:29:33 +0800

    feat：EFCore 仓储模式

--- aehyok.Schedules/Program.cs
+++ aehyok.Schedules/Program.cs
@@ -16,6 +16,8 @@ using Microsoft.Extensions.Configuration;
 using aehyok.Infrastructure.TypeFinders;
 using aehyok.EntityFramework.Entities;
 using System.Runtime.Loader;
+using aehyok.NCDP.Services;
+using Microsoft.Graph.Groups.Item.Sites.Item.GetApplicableContentTypesForListWithListId;
 
 Console.WriteLine("Hello, World!");
 
@@ -32,6 +34,9 @@ Thread.CurrentThread.Name = "aehyok.Schedules";
 
 builder.Services.AddMysql(builder.Configuration);
 
+builder.Services.AddServices();
+
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddRabbitMQ(builder.Configuration);
 
 builder.Services.AddCronServices();
@@ -46,5 +51,12 @@ service.Publish(new SelfReportPublishEvent()
     TaskId = 111111
 });
 
+var taskService = app.Services.GetRequiredService<ITaskService>();
+var list = await taskService.GetListAsync();
+
+foreach (var item in list)
+{
+    Console.WriteLine(item.Name);
+}
 // 运行主机
 await app.RunAsync();
--- aehyok.Schedules/ServiceCollectionExtensions.cs
+++ aehyok.Schedules/ServiceCollectionExtensions.cs
@@ -11,6 +11,8 @@ using aehyok.Infrastructure.TypeFinders;
 using aehyok.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using aehyok.EntityFramework.Repository;
+using aehyok.EntityFramework;
 namespace aehyok.Schedules
 {
     public static class ServiceCollectionExtensions
@@ -88,6 +90,33 @@ namespace aehyok.Schedules
                     //mysqlOptions.UseNetTopologySuite();
                 }).EnableSensitiveDataLogging().EnableDetailedErrors();
             }, poolSize: 1024);
+            services.AddScoped(typeof(IServiceBase<,>), typeof(ServiceBase<,>));
+            services.AddScoped(typeof(IServiceBase<>), typeof(ServiceBase<>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            var types = TypeFinders.SearchTypes(typeof(IScopedDependency), TypeFinders.TypeClassification.Interface);
+
+            foreach(var type in types)
+            {
+                var interfaces = type.GetInterfaces().Where(t => !t.IsGenericType && t != typeof(IScopedDependency));
+
+                foreach(var interfaceType in interfaces)
+                {
+                    services.AddService(interfaceType, type, ServiceLifetime.Scoped);
+                }
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddService(this IServiceCollection services, Type interfaceType, Type implementationType, ServiceLifetime lifetime)
+        {
+            services.Add(new ServiceDescriptor(implementationType, implementationType, lifetime));
+            services.Add(new ServiceDescriptor(interfaceType, implementationType, lifetime));
 
             return services;
         }
